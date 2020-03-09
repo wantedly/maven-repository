90a720d0a5bebe84e625815b1b1dff32397004b4
Masatoshi Kubode <masatoshi@wantedly.com>
2020-03-09 22:17:58 +0900

    fix dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,21 +23,16 @@ repositories {
 dependencies {
     // Align versions of all Kotlin components
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
-    // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-    // Use the Kotlin test library.
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-
-    // Use the Kotlin JUnit integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    implementation(kotlin("gradle-plugin"))
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
 }
 
 gradlePlugin {
     // Define the plugin
-    val greeting by plugins.creating {
-        id = "com.wantedly.maven.repository.greeting"
+    val mavenRepository by plugins.creating {
+        id = "com.wantedly.maven.repository"
         implementationClass = "com.wantedly.maven.repository.MavenRepositoryPlugin"
     }
 }
