34f736141ebd5d226e3bdda280f634554b0b85a1
Masatoshi Kubode <masatoshi@wantedly.com>
2020-03-10 21:50:26 +0900

    Add publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     `kotlin-dsl`
     id("org.jetbrains.kotlin.jvm") version "1.3.70"
+    id("com.jfrog.bintray") version "1.8.4"
     `maven-publish`
 }
 
@@ -18,4 +19,28 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit"))
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
+
+bintray {
+    user = System.getenv("BINTRAY_USER")
+    key = System.getenv("BINTRAY_KEY")
+    setPublications("maven")
+    pkg = PackageConfig().apply {
+        repo = "maven"
+        name = "${project.group}:${project.name}"
+        userOrg = "wantedly"
+        setLicenses("Apache-2.0")
+        vcsUrl = "https://github.com/wantedly/maven-repository.git"
+        version = VersionConfig().apply {
+            name = "${project.version}"
+        }
+    }
 }
